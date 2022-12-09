9b7ae2974a102f8f111fb1b86b4849a0bdc8e6ca
Khushaal2008 <75684230+Khushaal2008@users.noreply.github.com>
2022-12-09 20:51:17 +0530

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,47 @@
+AFRAME.registerComponent("markerhandler", {
+    init: async function()
+    {
+        this.el.addEventListener("markerFound", () => {
+            console.log("marker is found!");
+            this.handleMarkerFound();
+        });
+
+        this.el.addEventListener("markerLost", () => {
+            console.log("marker is Lost!");
+            this.handleMarkerLost();
+        })
+    },
+
+    handleMarkerFound: function()
+    {
+        var buttonDiv = document.getElementById("button-div");
+        buttonDiv.style.display = "flex";
+
+        var orderButton = document.getElementById("order-button");
+        var orderSummaryButton = document.getElementById("order-summary-button");
+
+        orderButton.addEventListener("click", () => {
+            SVGFEDropShadowElement({
+                icon: "https://i.imgur.com/4NZ6uLY.jpg",
+                title: "Thanks For Order!",
+                text: " ",
+                timer: 2000,
+                buttons:false
+            });
+        });
+
+        orderSummaryButton.addEventListener("click", () => {
+            swal({
+                icon: "warning",
+                title: "Order Summary",
+                text: "Work In Progress..."
+            })
+        })
+    }, 
+
+    handleMarkerLost: function () {
+        // Changing button div visibility
+        var buttonDiv = document.getElementById("button-div");
+        buttonDiv.style.display = "none";
+      }
+})
